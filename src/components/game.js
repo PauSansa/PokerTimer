ec9c75e72de25609622623c29efd36ec6dfce896
Pau Sansa <pausansa59@gmail.com>
2024-10-09 23:04:44 +0200

    improve show chips

--- src/components/game.js
+++ src/components/game.js
@@ -5,21 +5,25 @@ import { rounds } from "@/lib/round";
 import ShowRound from "@/components/showRound";
 import NextRounds from "@/components/nextRounds";
 import sleep from "@/util/sleep";
-import Chips from "@/components/chips";
 import { useSpeechSynthesis } from "react-speech-kit";
 import ButtonImage from "./ButtonImage/buttonImage";
+import ChipAmounts from "@/components/chipAmounts";
 
 export default function Game({ roundDuration }) {
   const { speak } = useSpeechSynthesis();
+
+  const [chipsIsOpen, setChipsIsOpen] = useState(false);
+  const [isChipsAnimating, setIsChipsAnimating] = useState(false);
+
+  const audioRef = useRef(null);
+  const [currentRound, setCurrentRound] = useState(rounds[0]);
+  const [announce, setAnnounce] = useState("tts");
+
   useEffect(() => {
     if (audioRef.current) {
       audioRef.current.volume = 0.8; // Establecer volumen al 80%
     }
   }, []);
-  const audioRef = useRef(null);
-  const [currentRound, setCurrentRound] = useState(rounds[0]);
-  const [showChips, setShowChips] = useState(false);
-  const [announce, setAnnounce] = useState("tts");
 
   function ttsNewRound(round) {
     speak({ text: `Ronda ${round.lvl}, Peque ${round.sb}, Big ${round.bb}` });
@@ -48,9 +52,26 @@ export default function Game({ roundDuration }) {
     }
   }
 
+  function openChipsModal() {
+    setIsChipsAnimating(false);
+    setChipsIsOpen(true);
+  }
+
+  function closeChipsModal() {
+    setIsChipsAnimating(true);
+    setTimeout(() => {
+      setChipsIsOpen(false);
+    }, 500); // Asumiendo que la duración de jump-out es 500ms
+  }
+
   return (
-    <div className="flex flex-col gap-4 w-11/12 mb-8 md:max-w-3xl relative">
-      {showChips && <Chips closeChips={() => setShowChips(false)} />}
+    <div className="flex md:mt-0 mt-6 flex-col gap-4 w-11/12 mb-8 md:max-w-3xl relative">
+      {chipsIsOpen && (
+        <ChipAmounts
+          closeChips={closeChipsModal}
+          isAnimating={isChipsAnimating}
+        />
+      )}
       <h1 className="md:text-5xl md:mb-6 font-bold text-3xl animate-wiggle mb-4 text-text z-0">
         Sansa&apos;s
         <br />
@@ -58,7 +79,7 @@ export default function Game({ roundDuration }) {
       </h1>
       <ButtonImage announce={announce} setAnnounce={setAnnounce} />
       <button
-        onClick={() => setShowChips(true)}
+        onClick={openChipsModal}
         className="p-2 bg-accent rounded-lg shadow-lg w-full text-black font-bold"
       >
         Show Chips
