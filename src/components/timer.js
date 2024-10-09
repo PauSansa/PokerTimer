701aaf5a22c8aa42e252b1f8619e20524044cc58
Pau Sansa <pausansa59@gmail.com>
2024-10-09 23:04:30 +0200

    improve UI

--- src/components/timer.js
+++ src/components/timer.js
@@ -14,37 +14,46 @@ function MyTimer({ roundDuration, nextRound }) {
       autoStart: false,
     });
 
-  const handleRestart = async () => {
-    await sleep(1000);
+  const handleNewRound = async () => {
+    await sleep(500);
     const time = new Date();
-    time.setMinutes(time.getMinutes() + roundDuration); // Reiniciar a 10 segundos
+    time.setMinutes(time.getMinutes() + roundDuration); // Reiniciar a la roundDuration inicial
     restart(time);
+  };
+
+  const handleRestart = async () => {
+    await handleNewRound();
+    pause();
   };
 
   const handleExpire = async () => {
     await nextRound();
-    handleRestart();
+    handleNewRound();
   };
 
   return (
     <div className="flex flex-col w-full bg-primary text-black items-center text-center justify-center rounded-lg shadow-lg gap-5 p-5">
-      <section className="flex flex-row justify-center items-center gap-2 rounded-lg w-full font-bold text-2xl">
+      <section className="flex flex-row justify-center items-center gap-2 rounded-lg w-full font-bold text-4xl">
         <p className="p-2 bg-gray-800/15 rounded-lg shadow-sm">{minutes}m</p>:
         <p className="p-2 bg-gray-800/15 rounded-lg shadow-sm">{seconds}s</p>
       </section>
-      <section className="flex flex-row gap-2">
-        <button
-          className="p-2 bg-gray-800/15 rounded-lg shadow-sm"
-          onClick={resume}
-        >
-          <PlayIcon width={30} height={30} />
-        </button>
-        <button
-          className="p-2 bg-gray-800/15 rounded-lg shadow-sm"
-          onClick={pause}
-        >
-          <PauseIcon width={30} height={30} />
-        </button>
+      <section className="flex flex-row gap-3">
+        {isRunning ? (
+          <button
+            className="p-2 bg-gray-800/15 rounded-lg shadow-sm"
+            onClick={pause}
+          >
+            <PauseIcon width={30} height={30} />
+          </button>
+        ) : (
+          <button
+            className="p-2 bg-gray-800/15 rounded-lg shadow-sm"
+            onClick={resume}
+          >
+            <PlayIcon width={30} height={30} />
+          </button>
+        )}
+
         <button
           className="p-2 bg-gray-800/15 rounded-lg shadow-sm"
           onClick={handleRestart}
--- src/lib/icons.js
+++ src/lib/icons.js
@@ -48,3 +48,20 @@ export const RestartIcon = (props) => (
     />
   </svg>
 );
+
+export const CloseIcon = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      stroke="#fff"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="m9 9 6 6m0-6-6 6m12-3a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+    />
+  </svg>
+);
